dd75b716250fbb2467446263fba39f4cd91093b6
MikemathuKE <michaelmathu6@gmail.com>
2025-07-11 13:37:20 +0300

    AI

--- ai.py
+++ ai.py
@@ -1,112 +1,108 @@
-import os
 import json
+import os
+import copy
 import random
 
-Q_TABLE_FILE = "q_table.json"
 WIN_REWARD = 1
 LOSE_REWARD = -1
 NO_REWARD = 0
-DISCOUNT_FACTOR = 0.9
-LEARNING_RATE = 0.1
+Q_TABLE = {}
 
 class AI:
-    def __init__(self, representation: str, train: bool = False, learning_rate=LEARNING_RATE, disount_factor=DISCOUNT_FACTOR, q_table_file: str = Q_TABLE_FILE):
-        self.q_table_file = q_table_file
-        self.load_q_table()
+    INITIAL_STATE_VALUE: int = 0
+    DISCOUNT_FACTOR: float = 0.9
+    LEARNING_RATE: float = 0.5
+
+    def __init__(self, value_rep: str, train: bool = False):
+        
+        self.load_qtable()
+        self.value_rep = value_rep
         self.train = train
-        self.name = "AI"
-        self.representation = representation
-        self.learning_rate = learning_rate
-        self.discount_factor = disount_factor
-        self.first_move = True
 
-    def load_q_table(self):
-        """Load the Q-table from a file."""
-        if os.path.exists(self.q_table_file):
-            with open(self.q_table_file, 'r') as file:
-                self.q_table = json.load(file)
-        else:
-            self.q_table = {}
-
-
-    def save_q_table(self):
-        """Save the Q-table to a file."""
-        with open(self.q_table_file, 'w') as file:
-            json.dump(self.q_table, file, indent=4)
-
-    def decode_state(self, board):
-        """Convert the board state into a string representation."""
-        representation = ""
-        for row in board:
-            for cell in row:
-                if cell is None:
-                    representation += '-'
+    def get_board_state(self, board: list[list]):
+        state = ""
+        for x in board:
+            for y in x:
+                if not y:
+                    state += "-"
                 else:
-                    representation += '1' if cell == self.representation else '0'
-        return representation
+                    if self.value_rep == y:
+                        state += "1"
+                    else:
+                        state += "0"
+        return state
     
-    def get_available_actions(self, board):
-        """Get a list of valid actions based on the current board state."""
-        return [(i, j) for i in range(len(board)) for j in range(len(board[i])) if board[i][j] is None]
+    def get_action_state(self, board: list[list], action: tuple[int]):
+        new_board = copy.deepcopy(board)
+        x, y = action
+        new_board[x][y] = self.value_rep
+        return self.get_board_state(new_board)
+
+    def get_possible_actions(self, board: list[list]):
+        possible_actions : list[tuple] = []
+        for x in range(3):
+            for y in range(3):
+                if board[x][y] == None:
+                    possible_actions.append((x, y))
+        return possible_actions
     
-    def random_action(self, board):
-        """Choose a random valid action from the available moves."""
-        available_moves = self.get_available_actions(board)
-        if available_moves:
-            selected=available_moves[random.randint(0, len(available_moves) - 1)]
-            return selected
-        
-        return None
-    
-    def decode_action(self, state, action):
-        """Convert an action tuple into a string representation."""
-        state_list = list(state)
-        state_list[action[0] * 3 + action[1]] = "1"
-        new_state = ''.join(state_list)
-        if new_state not in self.q_table:
-            self.q_table[new_state] = 0
-        return new_state
-    
-    def best_action(self, state, available_actions):
-        """Get the best action based on the Q-table for the current board state."""
-        
-        best_action = max(available_actions, key=lambda action: self.q_table.get(self.decode_action(state, action)))
+    def get_best_action(self, board, possible_actions):
+        best_value = -1
+        best_action: tuple = None
+
+        if len(possible_actions) == 1:
+            return possible_actions[0]
+
+        if self.train:
+            best_value = random.randint(0, len(possible_actions)-1)
+            best_action = possible_actions[best_value]
+        else:
+            for action in possible_actions:
+                action_state = self.get_action_state(board, action)
+
+                if not action_state in Q_TABLE.keys():
+                    Q_TABLE[action_state] = self.INITIAL_STATE_VALUE 
+
+
+                action_value = Q_TABLE[action_state]
+                if action_value > best_value:
+                    best_value = action_value
+                    best_action = action
         return best_action
     
-    def get_action(self, board):
-        """Get the best action for the current board state."""
-        state = self.decode_state(board)
-        if state not in self.q_table:
-            self.q_table[state] = 0
+    def get_next_move(self, board: list[list]):
+        current_state = self.get_board_state(board)
+        if not current_state in Q_TABLE.keys():
+            Q_TABLE[current_state] = self.INITIAL_STATE_VALUE
 
-        if self.train or self.first_move:
-            # Training mode: choose a random action
-            self.first_move = False
-            return self.random_action(board)
-        
+        possible_actions = self.get_possible_actions(board)
+        return self.get_best_action(board, possible_actions)
+    
+    def update_qtable(self, board: list[list], action: tuple[int], reward: int):
+        if self.train:
+            current_state = self.get_board_state(board)
+            if not current_state in Q_TABLE:
+                Q_TABLE[current_state] = self.INITIAL_STATE_VALUE
+            current_value = Q_TABLE[current_state]
+
+            new_state = self.get_action_state(board, action)
+            if not new_state in Q_TABLE:
+                Q_TABLE[new_state] = self.INITIAL_STATE_VALUE
+            new_state_value = Q_TABLE[new_state]
+
+            update_value = current_value + self.LEARNING_RATE * (reward + (self.DISCOUNT_FACTOR * new_state_value) - current_value )
+            Q_TABLE[current_state] = update_value
+
+            self.save_qtable()
+
+    def save_qtable(self):
+        with open("brain.json", "w") as file:
+            file.write(json.dumps(Q_TABLE))
+
+    def load_qtable(self):
+        if not os.path.exists("brain.json"):
+            loaded_qtable = {}
         else:
-            # Inference mode: choose the best action based on Q-values
-            available_actions = self.get_available_actions(board)
-            if not available_actions:
-                return None
-            
-            best_action = self.best_action(state, available_actions)
-            return best_action
-        
-    def update_q_table(self, board, action, reward):
-        """Update the Q-table based on the action taken and the reward received."""
-
-        state = self.decode_state(board)
-        if state not in self.q_table:
-            self.q_table[state] = 0
-
-        new_state = self.decode_action(state, action)
-        if new_state not in self.q_table:
-            self.q_table[new_state] = reward
-        
-        # Update the Q-value for the state-action pair
-        self.q_table[state] += self.learning_rate * (reward + self.discount_factor * self.q_table[new_state] - self.q_table[state])
-        
-        # Save the updated Q-table
-        self.save_q_table()
-
+            with open("brain.json", "r") as file:
+                loaded_qtable = json.loads(file.read())
+        Q_TABLE = loaded_qtable
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ If Python and Arcade are installed, this example can be run from the command lin
 python -m arcade.examples.starting_template
 """
 import arcade
-import ai
-import time
+from ai import AI, WIN_REWARD, LOSE_REWARD, NO_REWARD
+
 
 WINDOW_WIDTH = 900
 WINDOW_HEIGHT = 900
@@ -27,6 +27,7 @@ NUM_GRID = 3
 TRAIN_AI = False
 AI_ONE = True
 AI_TWO = False
+TRAINING_THRESHHOLD = 10000
 
 class MenuView(arcade.View):
     def on_show_view(self):
@@ -79,19 +80,20 @@ class PauseView(arcade.View):
             self.window.show_view(game)
 
 class GameOverView(arcade.View):
-    def __init__(self, game_view, draw=False):
+    def __init__(self, game_view, is_draw = False):
         super().__init__()
         self.game_view = game_view
-        self.draw = draw
+        self.is_draw = is_draw
 
     def on_show_view(self):
         self.window.background_color = arcade.color.ORANGE
 
     def on_draw(self):
         self.clear()
-        if self.draw:
-            arcade.draw_text("It's a Draw!", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2,
-                             arcade.color.BLACK, font_size=50, anchor_x="center")
+
+        if self.is_draw:
+            arcade.draw_text(f"IT'S A DRAW", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + 50,
+                            arcade.color.BLACK, font_size=50, anchor_x="center")
         else:
             arcade.draw_text(f"{self.game_view.curr_player} has Won!!", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + 50,
                             arcade.color.BLACK, font_size=50, anchor_x="center")
@@ -128,13 +130,22 @@ class GameView(arcade.View):
             [None, None, None]
         ]
         self.curr_player = "X"
-        
+        self.train = True
 
-        ai_player_one = ai.AI(representation="X", train=train, q_table_file="AI_X.json") if ai_one else None
-        ai_player_two = ai.AI(representation="O", train=train, q_table_file="AI_O.json") if ai_two else None
-        self.ai_players : list[ai.AI] = [ai_player_one, ai_player_two]
-        self.train = train
-        self.thinking = False
+        self.update_cycles = 0
+
+        self.ai_player = AI(value_rep="X", train=self.train)
+        
+        if self.train:
+            self.second_ai = AI(value_rep="O", train=self.train)
+            self.user_player = None
+        else:
+            self.user_player = "O"
+
+        self.prev_board = self.board
+        self.prev_action = None
+        # If you have sprite lists, you should create them here,
+        # and set them to None
 
     def change_player(self):
         if self.curr_player == "X":
@@ -146,11 +157,14 @@ class GameView(arcade.View):
         horizontal_match = False
         vertical_match = False
         diagonal_match = False
+        all_filled = True
 
         # Check for Horizontal Match
         for x in range(NUM_GRID):
             horizontal_match = False            
             for y in range(NUM_GRID):
+                if self.board[x][y] == None:
+                    all_filled = False
                 if self.board[x][y] == self.curr_player:
                     horizontal_match = True
                 else:
@@ -194,35 +208,37 @@ class GameView(arcade.View):
                             break
 
         if vertical_match or horizontal_match or diagonal_match:
+            if self.curr_player == self.ai_player.value_rep:
+                self.ai_player.update_qtable(self.prev_board, self.prev_action, WIN_REWARD)
+            else:
+                self.ai_player.update_qtable(self.prev_board, self.prev_action, LOSE_REWARD)
+
             if self.train:
-                self.reset_game()
+                self.reset()
             else:
                 gameoverview = GameOverView(self)
                 self.window.show_view(gameoverview)
             return True
-        elif all(cell is not None for row in self.board for cell in row):
-            # If all cells are filled and no winner, it's a draw
+        
+        self.ai_player.update_qtable(self.prev_board, self.prev_action, NO_REWARD)
+        if all_filled:
             if self.train:
-                self.reset_game()
+                self.reset()
             else:
-                gameoverview = GameOverView(self, draw=True)
+                gameoverview = GameOverView(self)
                 self.window.show_view(gameoverview)
-            return False
-    
         return False
-    
-    def reset_game(self):
-        """
-        Reset the game board and current player.
-        """
+        
+
+    def reset(self):
+        """Reset the game to the initial state."""
+        # Do changes needed to restart the game here if you want to support that
         self.board = [
             [None, None, None],
             [None, None, None],
             [None, None, None]
         ]
-        self.curr_player = "X"
-        self.thinking = False
-        self.window.show_view(self)
+        pass
 
     def on_draw(self):
         """
@@ -247,43 +263,39 @@ class GameView(arcade.View):
                 if self.board[x][2-y]:
                     arcade.draw_text(self.board[x][2-y], (x * SQUARE_WIDTH) + FONT_SPACING , (y * SQUARE_HEIGHT) + FONT_SPACING, arcade.color.YELLOW, FONT_SIZE)
 
-    def ai_thinking(self):
-        """
-        Simulate AI thinking time.
-        """
-        self.thinking = True
-        for ai_player in self.ai_players:
-            if ai_player and ai_player.representation == self.curr_player:
-                # AI's turn to play
-                action = ai_player.get_action(self.board)
-                if action:
-                    x, y = action
-                    prev_board = self.board.copy()
-                    if not self.board[x][y]:
-                        self.board[x][y] = self.curr_player
-                        if not self.has_player_won():
-                            if self.train:
-                                ai_player.update_q_table(prev_board, action, ai.NO_REWARD)                                
-                            self.change_player()
-                        else:
-                            if self.train:
-                                ai_player.update_q_table(prev_board, action, ai.WIN_REWARD)
-                                for other_ai_player in self.ai_players:
-                                    if other_ai_player and other_ai_player.representation != self.curr_player:
-                                        other_ai_player.update_q_table(prev_board, action, ai.LOSE_REWARD)
-
-                break
-        self.thinking = False
-
     def on_update(self, delta_time):
         """
         All the logic to move, and the game logic goes here.
         Normally, you'll call update() on the sprite lists that
         need it.
         """
-        
-        self.ai_thinking()
-        
+
+        if self.curr_player == self.ai_player.value_rep:
+            x, y = self.ai_player.get_next_move(self.board)
+            if not self.board[x][y]:
+                self.prev_board = self.board
+                self.prev_action = (x, y)
+                self.board[x][y] = self.curr_player
+                if not self.has_player_won():
+                    self.change_player()
+            self.ai_player.save_qtable()
+        if self.train:
+            if self.curr_player == self.second_ai.value_rep:
+                x, y = self.second_ai.get_next_move(self.board)
+                if not self.board[x][y]:
+                    self.prev_board = self.board
+                    self.prev_action = (x, y)
+                    self.board[x][y] = self.curr_player
+                    if not self.has_player_won():
+                        self.change_player()
+                self.second_ai.save_qtable()
+
+        if self.train:
+            self.update_cycles += 1
+
+            if self.update_cycles > TRAINING_THRESHHOLD:
+                gameoverview = GameOverView(self)
+                self.window.show_view(gameoverview)
 
     def on_key_press(self, key, key_modifiers):
         """
@@ -301,12 +313,15 @@ class GameView(arcade.View):
         """
         Called when the user presses a mouse button.
         """
-        if not self.thinking:
+
+        if self.curr_player == self.user_player:
             if button == arcade.MOUSE_BUTTON_LEFT:
                 for x_grid in range(NUM_GRID):
                     for y_grid in range(NUM_GRID):
                         if (x >= x_grid * SQUARE_WIDTH) and (x < (x_grid*SQUARE_WIDTH) + SQUARE_WIDTH) and (y >= y_grid * SQUARE_HEIGHT) and (y < (y_grid * SQUARE_HEIGHT)+ SQUARE_HEIGHT):
                             if not self.board[x_grid][2-y_grid]:
+                                self.prev_board = self.board
+                                self.prev_action = (x_grid, 2-y_grid)
                                 self.board[x_grid][2-y_grid] = self.curr_player
                                 if not self.has_player_won():
                                     self.change_player()
